a0d31bea26339f5a4f0a0a6058a1a95aeaa2312b
vishalkarad <vishalkarad777@gmail.com>
2020-04-15 21:05:22 +0530

    Modified : modified park method

--- src/main/java/com/bridgelabz/service/ParkingLotMain.java
+++ src/main/java/com/bridgelabz/service/ParkingLotMain.java
@@ -1,16 +1,22 @@
 package com.bridgelabz.service;
 
+import com.bridgelabz.VehiclePOJO;
 import com.bridgelabz.exception.ParkingLotException;
+import java.util.LinkedHashMap;
 
 public class ParkingLotMain {
 
     Object vehicle=null;
+    LinkedHashMap<String,Object> parkingLot = new LinkedHashMap<String, Object>();
 
-    // park vehicle
-    public void park(Object vehicle) throws ParkingLotException {
-        if (this.vehicle != null)
+    // park vehicle and check parking lot
+    public void park(VehiclePOJO vehicle) throws ParkingLotException {
+        if (parkingLot.size()%2==0 && parkingLot.size() != 0) {
+            parkingLot = null;
             throw new ParkingLotException("Parking lot is full");
+        }
         this.vehicle = vehicle;
+        parkingLot.put(vehicle.getVehicleNumber(),vehicle);
     }
 
     // Check Vehicle is present or not
@@ -21,13 +27,13 @@ public class ParkingLotMain {
     }
 
     // un park vehicle
-    public boolean unPark(Object vehicle) {
+    public boolean unPark(VehiclePOJO vehicle) {
         if (vehicle == null) return false;
         if (this.vehicle.equals(vehicle)){
             this.vehicle = null;
+            parkingLot.remove(vehicle.getVehicleNumber());
             return true;
         }
         else return false;
     }
-
 }
