0d6b751a634c0a730627c6404655bde99a3c5c36
vishalkarad <vishalkarad777@gmail.com>
2020-04-16 22:01:17 +0530

    Modified : set message when parking lot is full

--- src/main/java/com/bridgelabz/service/ParkingLotMain.java
+++ src/main/java/com/bridgelabz/service/ParkingLotMain.java
@@ -1,36 +1,38 @@
 package com.bridgelabz.service;
 
+import com.bridgelabz.AirportSecurity;
 import com.bridgelabz.VehiclePOJO;
 import com.bridgelabz.exception.ParkingLotException;
 import java.util.LinkedHashMap;
 
 public class ParkingLotMain {
 
-    Object vehicle=null;
     LinkedHashMap<String,Object> parkingLot = new LinkedHashMap<String, Object>();
+    AirportSecurity airportSecurity = new AirportSecurity();
 
     // park vehicle and check parking lot
-    public void park(VehiclePOJO vehicle) throws ParkingLotException {
+    public void park(VehiclePOJO vehicle) throws  ParkingLotException {
+        if (parkingLot.containsKey(vehicle.getVehicleNumber()))
+            throw new ParkingLotException(ParkingLotException.MyexceptionType.VEHICLE_ALREADY_PARK,"This vehicle already park");
         if (parkingLot.size()%2==0 && parkingLot.size() != 0) {
-            parkingLot = null;
-            throw new ParkingLotException("Parking lot is full");
+            parkingLot.put(vehicle.getVehicleNumber(),vehicle);
+            airportSecurity.setParkingSlotFullOrNot("parking lot is full");
+            throw new ParkingLotException(ParkingLotException.MyexceptionType.LOT_IS_FULL,airportSecurity.getParkingSlotFullOrNot());
+        }else {
+            parkingLot.put(vehicle.getVehicleNumber(),vehicle);
         }
-        this.vehicle = vehicle;
-        parkingLot.put(vehicle.getVehicleNumber(),vehicle);
     }
 
     // Check Vehicle is present or not
-    public boolean isVehiclePark(Object vehicle) {
-        if (this.vehicle.equals(vehicle))
+    public boolean isVehiclePark(VehiclePOJO vehicle) {
+        if (parkingLot.containsKey(vehicle.getVehicleNumber()))
             return true;
         return false;
     }
 
     // un park vehicle
     public boolean unPark(VehiclePOJO vehicle) {
-        if (vehicle == null) return false;
-        if (this.vehicle.equals(vehicle)){
-            this.vehicle = null;
+        if (parkingLot.containsKey(vehicle.getVehicleNumber())){
             parkingLot.remove(vehicle.getVehicleNumber());
             return true;
         }
