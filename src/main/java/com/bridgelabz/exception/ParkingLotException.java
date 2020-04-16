45e290ea994b262b3e7ab83c077b8bb73f8434b2
vishalkarad <vishalkarad777@gmail.com>
2020-04-16 21:14:38 +0530

    Added : add enum class and declare exception

--- src/main/java/com/bridgelabz/exception/ParkingLotException.java
+++ src/main/java/com/bridgelabz/exception/ParkingLotException.java
@@ -2,8 +2,15 @@ package com.bridgelabz.exception;
 
 public class ParkingLotException extends Exception{
 
-    public ParkingLotException(String message){
-        super(message);
+    public enum MyexceptionType  {
+        LOT_IS_FULL,VEHICLE_ALREADY_PARK;
     }
+    MyexceptionType type;
+
+    public ParkingLotException(MyexceptionType type,String message) {
+        super(message);
+        this.type = type;
+    }
+
 
 }
