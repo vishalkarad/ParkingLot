4466a3e0f2503de261064a23685f2390daa1650a
vishalkarad <vishalkarad777@gmail.com>
2020-04-16 21:10:10 +0530

    Added : add AirportSecurity class to inform when parking lot is full

---
+++ src/main/java/com/bridgelabz/AirportSecurity.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+public class AirportSecurity {
+
+    private String parkingSlotFullOrNot;
+
+    // setter method
+    public void setParkingSlotFullOrNot(String parkingSlotFullOrNot) {
+        this.parkingSlotFullOrNot = parkingSlotFullOrNot;
+    }
+
+    // getter method
+    public String getParkingSlotFullOrNot() {
+        return parkingSlotFullOrNot;
+    }
+}
