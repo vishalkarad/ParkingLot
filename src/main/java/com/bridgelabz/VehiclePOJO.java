02d60f6d60c65ecbd90dce8f51c6b6d0fa05abb8
vishalkarad <vishalkarad777@gmail.com>
2020-04-16 21:11:23 +0530

    Refactor : unwanted code deleted

--- src/main/java/com/bridgelabz/VehiclePOJO.java
+++ src/main/java/com/bridgelabz/VehiclePOJO.java
@@ -4,36 +4,30 @@ public class VehiclePOJO {
 
     private String vehicleName;
     private String vehicleNumber;
-    private String vehicleModel;
 
     // setter method
-    public void setVehicaleName(String vehicaleName) {
-        this.vehicleName = vehicaleName;
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
     }
     public void setVehicleNumber(String vehicleNumber) {
         this.vehicleNumber = vehicleNumber;
     }
-    public void setVehicleModel(String vehicleModel) {
-        this.vehicleModel = vehicleModel;
-    }
+
 
     // getter method
-    public String getVehicaleName() {
+    public String getVehicleName() {
         return vehicleName;
     }
     public String getVehicleNumber() {
         return vehicleNumber;
     }
-    public String getVehicleModel() {
-        return vehicleModel;
-    }
+
 
     @Override
     public String toString() {
         return "VehiclePOJO{" +
-                "vehicaleName='" + vehicleName + '\'' +
+                "vehicleName='" + vehicleName + '\'' +
                 ", vehicleNumber='" + vehicleNumber + '\'' +
-                ", vehicleModel='" + vehicleModel + '\'' +
                 '}';
     }
 }
