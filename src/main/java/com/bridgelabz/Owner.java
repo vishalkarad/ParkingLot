9be8694022123476302605d5889a34ee96bb298d
vishalkarad <vishalkarad777@gmail.com>
2020-04-17 10:35:13 +0530

    Added : add owner class to inform parking lot is full or not

---
+++ src/main/java/com/bridgelabz/Owner.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class Owner {
+    private String parkingFullOrNot;
+
+    // setter method
+    public void setParkingFullOrNot(String parkingFullOrNot) {
+        this.parkingFullOrNot = parkingFullOrNot;
+    }
+
+    // getter method
+    public String getParkingFullOrNot() {
+        return parkingFullOrNot;
+    }
+}
