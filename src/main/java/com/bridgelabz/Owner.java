6cd7b61ee3579828f5022e91a24045ecf22b8a76
vishalkarad <vishalkarad777@gmail.com>
2020-04-17 13:22:32 +0530

    Added : declare variable and create set mehod to set parking charg

--- src/main/java/com/bridgelabz/Owner.java
+++ src/main/java/com/bridgelabz/Owner.java
@@ -2,10 +2,14 @@ package com.bridgelabz;
 
 public class Owner {
     private String parkingFullOrNot;
+    private String parkingCharge;
 
     // setter method
     public void setParkingFullOrNot(String parkingFullOrNot) {
         this.parkingFullOrNot = parkingFullOrNot;
+    }
+    public void setParkingCharge(String parkingCharge) {
+        this.parkingCharge = parkingCharge;
     }
 
     // getter method
