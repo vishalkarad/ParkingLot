707dfefca09e5cb6e0e00f09bd3985d750304c6a
vishalkarad <vishalkarad777@gmail.com>
2020-04-21 19:18:15 +0530

    Added : add variable vehicleType and setter method

--- src/main/java/com/bridgelabz/VehiclePOJO.java
+++ src/main/java/com/bridgelabz/VehiclePOJO.java
@@ -1,9 +1,14 @@
 package com.bridgelabz;
 
+import com.bridgelabz.service.ParkingLotMain;
+
+import java.util.Comparator;
+
 public class VehiclePOJO {
 
     private String vehicleName;
     private String vehicleNumber;
+    public String vehicleType;
 
     // setter method
     public void setVehicleName(String vehicleName) {
@@ -12,7 +17,9 @@ public class VehiclePOJO {
     public void setVehicleNumber(String vehicleNumber) {
         this.vehicleNumber = vehicleNumber;
     }
-
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
 
     // getter method
     public String getVehicleName() {
