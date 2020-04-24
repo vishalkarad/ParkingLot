604858b5aa752d51cb33444ba704e6550108de30
vishalkarad <vishalkarad777@gmail.com>
2020-04-24 22:27:32 +0530

    ADDED : search all vehicle between start and end time

--- src/main/java/com/bridgelabz/utilities/CalculateTime.java
+++ src/main/java/com/bridgelabz/utilities/CalculateTime.java
@@ -1,12 +1,37 @@
-package com.bridgelabz;
-
+package com.bridgelabz.utilities;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CalculateTime {
+    public Map<Integer,Object> vehicleTime = new HashMap<>();
+    String location="";
+
+    public CalculateTime(Map<Integer, Object> vehicleTime) {
+        this.vehicleTime = vehicleTime;
+    }
+
     public static String getCurrentTime() {
         Calendar cal = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
         return sdf.format(cal.getTime());
+    }
+    public String vehicleIntime(String... contains) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+        Date startTime = format.parse(contains[0]);
+        Date endTime = format.parse(contains[1]);
+        try {
+            for (int vehicleKey=1;vehicleKey<=vehicleTime.size();vehicleKey++) {
+                Date userDate = format.parse(vehicleTime.get(vehicleKey).toString());
+                if (userDate.after(startTime) && userDate.before(endTime))
+                    location += vehicleKey +",";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return location;
     }
 }
